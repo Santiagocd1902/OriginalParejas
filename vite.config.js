666129a2cb4f6a615b1de9c7eff3818301224737
Santiagocd1902 <david.castellanosd@uniagustiniana.edu.co>
2025-10-25 19:04:49 +0000

    fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// 👇 ESTA ES LA CLAVE: usar rutas relativas
 export default defineConfig({
+  base: "./",
   plugins: [react()],
 })
